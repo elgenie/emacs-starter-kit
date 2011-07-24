37484b3527e16a57012b6a1a3591c13dd73bceea
Eugene Letuchy <eletuchy@fb.com>
2011-07-24 04:28:54 -0700

    fix bug with before-make-frame-hook when emacsclient starts emacs --daemon

--- starter-kit-defuns.el
+++ starter-kit-defuns.el
@@ -34,15 +34,15 @@ Symbols matching the text at point are put first in the completion list."
                              (cond
                               ((and (listp symbol) (imenu--subalist-p symbol))
                                (addsymbols symbol))
-                              
+
                               ((listp symbol)
                                (setq name (car symbol))
                                (setq position (cdr symbol)))
-                              
+
                               ((stringp symbol)
                                (setq name symbol)
                                (setq position (get-text-property 1 'org-imenu-marker symbol))))
-                             
+
                              (unless (or (null position) (null name))
                                (add-to-list 'symbol-names name)
                                (add-to-list 'name-and-pos (cons name position))))))))
@@ -90,7 +90,7 @@ Symbols matching the text at point are put first in the completion list."
   (paredit-mode t))
 
 (defun turn-off-tool-bar ()
-  (tool-bar-mode -1))
+  (if (fboundp 'tool-bar-mode) (tool-bar-mode -1)))
 
 (defun turn-on-idle-highlight ()
   (idle-highlight-mode t))
@@ -107,7 +107,7 @@ Symbols matching the text at point are put first in the completion list."
 (add-hook 'coding-hook 'pretty-lambdas)
 (add-hook 'coding-hook 'add-watchwords)
 (add-hook 'coding-hook 'turn-on-idle-highlight)
-  
+
 (defun run-coding-hook ()
   "Enable things that are convenient across all coding buffers."
   (run-hooks 'coding-hook))
