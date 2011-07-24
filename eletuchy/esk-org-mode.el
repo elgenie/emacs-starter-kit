79aacefa2cd0036c34db2cfe6ac860bed123792a
Eugene Letuchy <eletuchy@fb.com>
2011-07-24 04:08:45 -0700

    renaming org.el to esk-org.el

--- eletuchy/esk-org-mode.el
+++ eletuchy/esk-org-mode.el
@@ -2,6 +2,7 @@
 (add-hook 'org-mode-hook '(lambda () (visual-line-mode t)))
 (setq org-mobile-inbox-for-pull (concat org-directory "/inbox.org"))
 (setq org-log-done 'time)
+(setq org-hide-leading-stars t)
 (setq org-todo-keywords
       '((type "TODO(t)" "STARTED(s)" "WAITING(w)" "APPT(a)" "|" "CANCELLED(c)" "DEFERRED(e)" "DONE(d)")
         (sequence "PROJECT(p)" "|" "FINISHED(f)")
